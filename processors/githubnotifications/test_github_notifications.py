361326c68e569dc8329e6669ca3146fe1f5ea75f
Paul Hammant <paul@hammant.org>
2016-04-03 20:53:57 -0400

    terser

--- processors/githubnotifications/test_github_notifications.py
+++ processors/githubnotifications/test_github_notifications.py
@@ -7,6 +7,18 @@ from mockextras import stub, Any
 from digester import Digester
 from processors.githubnotifications.github_notification_processor import GithubNotificationProcessor
 
+MAIL_HDR = """From: P H <ph@example.com>
+Content-Transfer-Encoding: 8bit
+Content-Type: multipart/alternative; boundary="---NOTIFICATION_BOUNDARY"
+MIME-Version: 1.0
+This is a multi-part message in MIME format.
+-----NOTIFICATION_BOUNDARY
+Content-Type: text/html; charset="utf-7"
+Content-Transfer-Encoding: utf-7
+
+
+"""
+
 
 class NotifsStore(object):
 
@@ -35,28 +47,28 @@ class TestGithubNotifications(TestCase):
     def test_two_related_notifs_can_be_rolled_up(self):
 
         expected_payload = """<table>
-  <tr style="background-color: #acf;">
-    <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
-  </tr>
-  <tr style="">
-    <td valign="top">Apr 02 2016<br/>03:14 AM</td>
-    <td>
-      <table style="border-top: none">
-        <tr>
-          <td style="border-bottom: 2px solid lightgrey;">
-            <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
-          </td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold;">dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
-        </tr>
-       </table>
-    </td>
-  </tr>
-</table>"""
+            <tr style="background-color: #acf;">
+              <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
+            </tr>
+            <tr style="">
+              <td valign="top">Apr 02 2016<br/>03:14 AM</td>
+              <td>
+                <table style="border-top: none">
+                  <tr>
+                    <td style="border-bottom: 2px solid lightgrey;">
+                      <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight: bold;">dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
+                  </tr>
+                 </table>
+              </td>
+            </tr>
+          </table>""".replace("\n          ","\n")
 
         notification_store = {}
 
@@ -72,20 +84,7 @@ class TestGithubNotifications(TestCase):
             (call('most-recently-seen', 0), True)
         )
 
-        expected_message = \
-"""From: P H <ph@example.com>
-Content-Transfer-Encoding: 8bit
-Content-Type: multipart/alternative; boundary="---NOTIFICATION_BOUNDARY"
-MIME-Version: 1.0
-This is a multi-part message in MIME format.
------NOTIFICATION_BOUNDARY
-Content-Type: text/html; charset="utf-7"
-Content-Transfer-Encoding: utf-7
-
-
-"""
-
-        expected_message = "Subject: Github Rollup (2 new)\n" + expected_message + expected_payload
+        expected_message = "Subject: Github Rollup (2 new)\n" + MAIL_HDR + expected_payload
 
         rollup_inbox_proxy = Mock()
         rollup_inbox_proxy.delete_previous_message.side_effect = stub((call(), True))
@@ -100,12 +99,9 @@ Content-Transfer-Encoding: utf-7
         unmatched_to_move = []
         to_delete_from_inqueue = []
 
-        with open('testdata/github_1.txt', 'r') as myfile:
-            notification_1_content = myfile.read().replace('\n', '\r\n')
-        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
+        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
 
-        with open('testdata/github_2.txt', 'r') as myfile:
-            notification_2_content = myfile.read().replace('\n', '\r\n')
+        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
         digester.process_incoming_message(1235, processors, notification_2_content, to_delete_from_inqueue, unmatched_to_move, False)
 
         processor.rewrite_rollup_emails(rollup_inbox_proxy, has_previous_message=True,
@@ -141,48 +137,55 @@ Content-Transfer-Encoding: utf-7
         self.assertEquals(str(to_delete_from_inqueue), "[1234, 1235]")
         self.assertEquals(len(final_notifs_store.notifs), 1)
 
+    def get_gh_emailed_notifications(self):
+        with open('testdata/github_1.txt', 'r') as myfile:
+            notification_1_content = myfile.read().replace('\n', '\r\n')
+        with open('testdata/github_2.txt', 'r') as myfile:
+            notification_2_content = myfile.read().replace('\n', '\r\n')
+        return notification_1_content, notification_2_content
+
     def test_two_related_notifis_can_be_rolled_up_with_a_prior_notification(self):
 
         expected_payload = """<span>You have previously read notifications up to: Apr 01 2016 09:20 PM</span>
-<table>
-  <tr style="background-color: #acf;">
-    <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
-  </tr>
-  <tr style="">
-    <td valign="top">Apr 02 2016<br/>03:14 AM</td>
-    <td>
-      <table style="border-top: none">
-        <tr>
-          <td style="border-bottom: 2px solid lightgrey;">
-            <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
-          </td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold;">dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
-        </tr>
-       </table>
-    </td>
-  </tr>
-  <tr><td colspan="2" style="border-bottom: 1pt solid red; border-top: 1pt solid red;"><center>^ New/Updated Notifications Since You Last Checked ^</center></td></tr>
-  <tr style="background-color: #def;">
-    <td valign="top">Apr 01 2016<br/>09:20 PM</td>
-    <td>
-      <table style="border-top: none">
-        <tr>
-          <td style="border-bottom: 2px solid lightgrey;">
-            <a href="https://github.com/aaa/bbb/pull/1111">Pull Request: [aaa/bbb] ired 0.5.0 (#1234)</a>
-          </td>
-        </tr>
-        <tr>
-          <td >xxx: x xx (comment) Hello, how are you?</td>
-        </tr>
-       </table>
-    </td>
-  </tr>
-</table>"""
+          <table>
+            <tr style="background-color: #acf;">
+              <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
+            </tr>
+            <tr style="">
+              <td valign="top">Apr 02 2016<br/>03:14 AM</td>
+              <td>
+                <table style="border-top: none">
+                  <tr>
+                    <td style="border-bottom: 2px solid lightgrey;">
+                      <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight: bold;">dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
+                  </tr>
+                 </table>
+              </td>
+            </tr>
+            <tr><td colspan="2" style="border-bottom: 1pt solid red; border-top: 1pt solid red;"><center>^ New/Updated Notifications Since You Last Checked ^</center></td></tr>
+            <tr style="background-color: #def;">
+              <td valign="top">Apr 01 2016<br/>09:20 PM</td>
+              <td>
+                <table style="border-top: none">
+                  <tr>
+                    <td style="border-bottom: 2px solid lightgrey;">
+                      <a href="https://github.com/aaa/bbb/pull/1111">Pull Request: [aaa/bbb] ired 0.5.0 (#1234)</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td >xxx: x xx (comment) Hello, how are you?</td>
+                  </tr>
+                 </table>
+              </td>
+            </tr>
+          </table>""".replace("\n          ","\n")
 
         initial_notification_store = {
             'aaa/bbb/pull/1111@github.com': {
@@ -211,20 +214,7 @@ Content-Transfer-Encoding: utf-7
             (call('most-recently-seen', 1459560000), True)
         )
 
-        expected_message = \
-"""From: P H <ph@example.com>
-Content-Transfer-Encoding: 8bit
-Content-Type: multipart/alternative; boundary="---NOTIFICATION_BOUNDARY"
-MIME-Version: 1.0
-This is a multi-part message in MIME format.
------NOTIFICATION_BOUNDARY
-Content-Type: text/html; charset="utf-7"
-Content-Transfer-Encoding: utf-7
-
-
-"""
-
-        expected_message = "Subject: Github Rollup (2 new)\n" + expected_message + expected_payload
+        expected_message = "Subject: Github Rollup (2 new)\n" + MAIL_HDR + expected_payload
 
         rollup_inbox_proxy = Mock()
         rollup_inbox_proxy.delete_previous_message.side_effect = stub((call(), True))
@@ -239,12 +229,9 @@ Content-Transfer-Encoding: utf-7
         unmatched_to_move = []
         to_delete_from_inqueue = []
 
-        with open('testdata/github_1.txt', 'r') as myfile:
-            notification_1_content = myfile.read().replace('\n', '\r\n')
-        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
+        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
 
-        with open('testdata/github_2.txt', 'r') as myfile:
-            notification_2_content = myfile.read().replace('\n', '\r\n')
+        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
         digester.process_incoming_message(1235, processors, notification_2_content, to_delete_from_inqueue, unmatched_to_move, False)
 
         processor.rewrite_rollup_emails(rollup_inbox_proxy, has_previous_message=True,
@@ -295,29 +282,29 @@ Content-Transfer-Encoding: utf-7
     def test_two_related_notifs_can_be_rolled_up_where_one_was_previously_seen(self):
 
         expected_payload = """<span>You have previously read notifications up to: Apr 02 2016 02:14 AM</span>
-<table>
-  <tr style="background-color: #acf;">
-    <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
-  </tr>
-  <tr style="">
-    <td valign="top">Apr 02 2016<br/>03:14 AM</td>
-    <td>
-      <table style="border-top: none">
-        <tr>
-          <td style="border-bottom: 2px solid lightgrey;">
-            <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
-          </td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
-        </tr>
-        <tr>
-          <td >dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
-        </tr>
-       </table>
-    </td>
-  </tr>
-</table>"""
+          <table>
+            <tr style="background-color: #acf;">
+              <th>When</th><th>Issues/Pull Requests &amp; Their Notifications</th>
+            </tr>
+            <tr style="">
+              <td valign="top">Apr 02 2016<br/>03:14 AM</td>
+              <td>
+                <table style="border-top: none">
+                  <tr>
+                    <td style="border-bottom: 2px solid lightgrey;">
+                      <a href="https://github.com/Homebrew/homebrew/pull/50441">Pull Request: [Homebrew/homebrew] ired 0.5.0 (#50441)</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="font-weight: bold;">ppiper: Peter Piper (comment) Peter Piper picked a peck of pickled peppers....</td>
+                  </tr>
+                  <tr>
+                    <td >dholm: David Holm (comment 60.0 mins earlier) [quoted block] @dunn Fixed....</td>
+                  </tr>
+                 </table>
+              </td>
+            </tr>
+          </table>""".replace("\n          ","\n")
 
         notification_store = {}
 
@@ -333,20 +320,7 @@ Content-Transfer-Encoding: utf-7
             (call('most-recently-seen', 1459577657), True)
         )
 
-        expected_message = \
-"""From: P H <ph@example.com>
-Content-Transfer-Encoding: 8bit
-Content-Type: multipart/alternative; boundary="---NOTIFICATION_BOUNDARY"
-MIME-Version: 1.0
-This is a multi-part message in MIME format.
------NOTIFICATION_BOUNDARY
-Content-Type: text/html; charset="utf-7"
-Content-Transfer-Encoding: utf-7
-
-
-"""
-
-        expected_message = "Subject: Github Rollup (1 new)\n" + expected_message + expected_payload
+        expected_message = "Subject: Github Rollup (1 new)\n" + MAIL_HDR + expected_payload
 
         rollup_inbox_proxy = Mock()
         rollup_inbox_proxy.delete_previous_message.side_effect = stub((call(), True))
@@ -361,12 +335,9 @@ Content-Transfer-Encoding: utf-7
         unmatched_to_move = []
         to_delete_from_inqueue = []
 
-        with open('testdata/github_1.txt', 'r') as myfile:
-            notification_1_content = myfile.read().replace('\n', '\r\n')
-        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
+        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
 
-        with open('testdata/github_2.txt', 'r') as myfile:
-            notification_2_content = myfile.read().replace('\n', '\r\n')
+        digester.process_incoming_message(1234, processors, notification_1_content, to_delete_from_inqueue, unmatched_to_move, False)
         digester.process_incoming_message(1235, processors, notification_2_content, to_delete_from_inqueue, unmatched_to_move, False)
 
         processor.rewrite_rollup_emails(rollup_inbox_proxy, has_previous_message=True,
