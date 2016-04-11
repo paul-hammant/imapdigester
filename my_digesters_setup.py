6348954b9d9ab61e0227bda32388041c23876912
Paul Hammant <paul@hammant.org>
2016-04-11 08:22:09 -0400

    documentation

--- my_digesters_setup.py
+++ my_digesters_setup.py
@@ -8,7 +8,11 @@ from metastore import MetaStore
 from digesters.charges.charge_card_digester import ChargeCardDigester
 
 def add_digesters(digesters):
+
+    # StackExchange filtered notifications
     digesters.append(StackExchangeNotificationDigester(MetaStore("stack_exchange_1"), "My SO filters"))
+
+    # Charge (Credit) card spenting alerts (and mmore)
     digesters.append(ChargeCardDigester(MetaStore("charge_cards"))
                       .with_amex()
                       .with_chase()
@@ -17,7 +21,22 @@ def add_digesters(digesters):
                       .with_jpmorgan()
                       .with_capitalone()
                       .with_citi())
+
+    # Regular Github.com notifications (you are watching a repo)
     digesters.append(GithubNotificationDigester(MetaStore("github_notifications")))
+
+    # Github Enterprise notifications (you are watching a repo)
+    digesters.append(GithubNotificationDigester(MetaStore("ghe_notifications"),
+                                            return_path_email="noreply-ghe@yourcompany.com",
+                                            from_email="ghe-notifications@yourcompany.com",
+                                            site="ghe.yourcompany.com",
+                                            known_as="GHE"))
+
+    # HipChat notifications
     digesters.append(HipchatNotificationDigester(MetaStore("hipchat_notifications")))
+
+    # Linkedin Invitations
     digesters.append(LinkedinInvitationDigester(MetaStore("linkedin_invitations")))
+
+    # Confluence Notifications
     digesters.append(ConfluenceNotificationDigester(MetaStore("confluence_notifications")))
