627e4a7072bd1c7499c616c621ff7197489f3d5f
Paul Hammant <paul@hammant.org>
2016-04-25 22:18:54 -0400

    inline test data

--- digesters/github/test_github_notifications.py
+++ digesters/github/test_github_notifications.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sys
 from unittest import TestCase
 
@@ -101,10 +102,8 @@ class TestGithubNotifications(TestCase):
         unmatched_to_move = []
         to_delete_from_notification_folder = []
 
-        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
-
-        digestion_processor.process_incoming_notification(1234, digesters, notification_1_content, to_delete_from_notification_folder, unmatched_to_move, False)
-        digestion_processor.process_incoming_notification(1235, digesters, notification_2_content, to_delete_from_notification_folder, unmatched_to_move, False)
+        digestion_processor.process_incoming_notification(1234, digesters, GITHUB_1, to_delete_from_notification_folder, unmatched_to_move, False)
+        digestion_processor.process_incoming_notification(1235, digesters, GITHUB_2, to_delete_from_notification_folder, unmatched_to_move, False)
 
         digester.rewrite_digest_emails(digest_inbox_proxy, has_previous_message=True,
                                        previously_seen=False, sender_to_implicate="ph@example.com")
@@ -138,13 +137,6 @@ class TestGithubNotifications(TestCase):
         self.assertEquals(len(unmatched_to_move), 0)
         self.assertEquals(str(to_delete_from_notification_folder), "[1234, 1235]")
         self.assertEquals(len(final_notifs_store.notifs), 1)
-
-    def get_gh_emailed_notifications(self):
-        with open('../../testdata/github_1.txt', 'r') as myfile:
-            notification_1_content = myfile.read().replace('\n', '\r\n')
-        with open('../../testdata/github_2.txt', 'r') as myfile:
-            notification_2_content = myfile.read().replace('\n', '\r\n')
-        return notification_1_content, notification_2_content
 
     def test_two_related_notifis_can_be_rolled_up_with_a_prior_notification(self):
 
@@ -216,7 +208,7 @@ class TestGithubNotifications(TestCase):
             (call('most-recently-seen', 1459560000), True)
         )
 
-        expected_message = "Subject: Github Digest (2 new)\n" + MAIL_HDR + expected_payload \
+        expected_message = "Subject: Watched Repositories Digest (2 new)\n" + MAIL_HDR + expected_payload \
                            + '\n\n-----NOTIFICATION_BOUNDARY-5678'
 
         digest_inbox_proxy = Mock()
@@ -224,7 +216,7 @@ class TestGithubNotifications(TestCase):
         digest_inbox_proxy.append.side_effect = stub((call(expected_message), True))
 
         digesters = []
-        digester = GithubNotificationDigester(store_writer)  ## What we are testing
+        digester = GithubNotificationDigester(store_writer, known_as="Our inhouse GH")  ## What we are testing
         digester.notification_boundary_rand = "-5678"  # no random number for the email's notification boundary
         digesters.append(digester)
 
@@ -233,10 +225,8 @@ class TestGithubNotifications(TestCase):
         unmatched_to_move = []
         to_delete_from_notification_folder = []
 
-        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
-
-        digester_processor.process_incoming_notification(1234, digesters, notification_1_content, to_delete_from_notification_folder, unmatched_to_move, False)
-        digester_processor.process_incoming_notification(1235, digesters, notification_2_content, to_delete_from_notification_folder, unmatched_to_move, False)
+        digester_processor.process_incoming_notification(1234, digesters, GITHUB_1, to_delete_from_notification_folder, unmatched_to_move, False)
+        digester_processor.process_incoming_notification(1235, digesters, GITHUB_2, to_delete_from_notification_folder, unmatched_to_move, False)
 
         digester.rewrite_digest_emails(digest_inbox_proxy, has_previous_message=True,
                                        previously_seen=True, sender_to_implicate="ph@example.com")
@@ -324,7 +314,7 @@ class TestGithubNotifications(TestCase):
             (call('most-recently-seen', 1459577657), True)
         )
 
-        expected_message = "Subject: Github Watched Repos Digest (1 new)\n" + MAIL_HDR + expected_payload \
+        expected_message = "Subject: Watched Repositories Digest (1 new)\n" + MAIL_HDR + expected_payload \
                            + '\n\n-----NOTIFICATION_BOUNDARY-5678'
 
         digest_inbox_proxy = Mock()
@@ -332,7 +322,7 @@ class TestGithubNotifications(TestCase):
         digest_inbox_proxy.append.side_effect = stub((call(expected_message), True))
 
         digesters = []
-        digester = GithubNotificationDigester(store_writer)  ## What we are testing
+        digester = GithubNotificationDigester(store_writer, known_as="Our inhouse GH")  ## What we are testing
         digester.notification_boundary_rand = "-5678"  # no random number for the email's notification boundary
         digesters.append(digester)
 
@@ -341,10 +331,8 @@ class TestGithubNotifications(TestCase):
         unmatched_to_move = []
         to_delete_from_notification_folder = []
 
-        notification_1_content, notification_2_content = self.get_gh_emailed_notifications()
-
-        digester_processor.process_incoming_notification(1234, digesters, notification_1_content, to_delete_from_notification_folder, unmatched_to_move, False)
-        digester_processor.process_incoming_notification(1235, digesters, notification_2_content, to_delete_from_notification_folder, unmatched_to_move, False)
+        digester_processor.process_incoming_notification(1234, digesters, GITHUB_1, to_delete_from_notification_folder, unmatched_to_move, False)
+        digester_processor.process_incoming_notification(1235, digesters, GITHUB_2, to_delete_from_notification_folder, unmatched_to_move, False)
 
         digester.rewrite_digest_emails(digest_inbox_proxy, has_previous_message=True,
                                        previously_seen=False, sender_to_implicate="ph@example.com")
@@ -378,3 +366,104 @@ class TestGithubNotifications(TestCase):
         self.assertEquals(len(unmatched_to_move), 0)
         self.assertEquals(str(to_delete_from_notification_folder), "[1234, 1235]")
         self.assertEquals(len(final_notifs_store.notifs), 1)
+
+GITHUB_1 = """Date: Sat, 2 Apr 2016 06:36:07 -0700
+From: David Holm <notifications@github.com>
+To: Homebrew/homebrew <homebrew@noreply.github.com>
+Message-ID: <Homebrew/homebrew/pull/50441/c204717687@github.com>
+In-Reply-To: <Homebrew/homebrew/pull/50441@github.com>
+References: <Homebrew/homebrew/pull/50441@github.com>
+Subject: Re: [Homebrew/homebrew] ired 0.5.0 (#50441)
+Content-Type: multipart/alternative;
+	boundary="--==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528";
+	charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+Precedence: list
+X-GitHub-Sender: dholm
+X-GitHub-Recipient: paul-hammant
+Return-Path: noreply@github.com
+X-OriginalArrivalTime: 02 Apr 2016 06:14:16.0870 (UTC) FILETIME=[52CF5460:01E18CF2]
+MIME-Version: 1.0
+
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528
+Content-Type: text/plain; charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+
+> something quotes
+> something else
+
+@dunn Fixed.                                           (past col 55)
+a
+
+---
+You are receiving this because you are subscribed to this thread.
+Reply to this email directly or view it on GitHub:
+https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717687
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528
+Content-Type: text/html; charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><p><a href="https://github.com/dunn" class="user-mention">@dunn</a> Fixed.</p>
+
+<p style="font-size:small;-webkit-text-size-adjust:none;color:#666;">—<br>You are receiving this because you are subscribed to this thread.<br>Reply to this email directly or <a href="https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717687">view it on GitHub</a><img alt="" height="1" src="https://github.com/notifications/beacon/AAFBBk8Ip8lbw1L2458lplBtMpFU4Iknks5pznDHgaJpZM4H5RV8.gif" width="1"></p>
+<div itemscope="" itemtype="http://schema.org/EmailMessage">
+<div itemprop="action" itemscope="" itemtype="http://schema.org/ViewAction">
+  <link itemprop="url" href="https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717687"><!-- </link> -->
+  <meta itemprop="name" content="View Pull Request"><!-- </meta> -->
+</div>
+<meta itemprop="description" content="View this Pull Request on GitHub"><!-- </meta> -->
+</div>
+
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528--"""
+
+
+GITHUB_2 = """Date: Sat, 2 Apr 2016 07:36:07 -0700
+From: Peter Piper <notifications@github.com>
+To: Homebrew/homebrew <homebrew@noreply.github.com>
+Message-ID: <Homebrew/homebrew/pull/50441/c204717689@github.com>
+In-Reply-To: <Homebrew/homebrew/pull/50441@github.com>
+References: <Homebrew/homebrew/pull/50441@github.com>
+Subject: Re: [Homebrew/homebrew] ired 0.5.0 (#50441)
+Content-Type: multipart/alternative;
+	boundary="--==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528";
+	charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+Precedence: list
+X-GitHub-Sender: ppiper
+X-GitHub-Recipient: paul-hammant
+Return-Path: noreply@github.com
+X-OriginalArrivalTime: 02 Apr 2016 07:14:16.0870 (UTC) FILETIME=[52CF5460:01E18CF2]
+MIME-Version: 1.0
+
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528
+Content-Type: text/plain; charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+
+Peter Piper picked a peck of pickled peppers.
+A peck of pickled peppers Peter Piper picked.
+If Peter Piper picked a peck of pickled peppers,
+Where's the peck of pickled peppers that Peter Piper picked?
+
+---
+You are receiving this because you are subscribed to this thread.
+Reply to this email directly or view it on GitHub:
+https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717688
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528
+Content-Type: text/html; charset="UTF-8"
+Content-Transfer-Encoding: 7bit
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><p>Peter Piper picked a peck of pickled peppers.
+A peck of pickled peppers Peter Piper picked.
+If Peter Piper picked a peck of pickled peppers,
+Where's the peck of pickled peppers that Peter Piper picked?</p>
+
+<p style="font-size:small;-webkit-text-size-adjust:none;color:#666;">—<br>You are receiving this because you are subscribed to this thread.<br>Reply to this email directly or <a href="https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717687">view it on GitHub</a><img alt="" height="1" src="https://github.com/notifications/beacon/AAFBBk8Ip8lbw1L2458lplBtMpFU4Iknks5pznDHgaJpZM4H5RV8.gif" width="1"></p>
+<div itemscope="" itemtype="http://schema.org/EmailMessage">
+<div itemprop="action" itemscope="" itemtype="http://schema.org/ViewAction">
+  <link itemprop="url" href="https://github.com/Homebrew/homebrew/pull/50441#issuecomment-204717687"><!-- </link> -->
+  <meta itemprop="name" content="View Pull Request"><!-- </meta> -->
+</div>
+<meta itemprop="description" content="View this Pull Request on GitHub"><!-- </meta> -->
+</div>
+
+----==_mimepart_56ffcac7e82a2_9a33f9431ac72b8233528--"""
